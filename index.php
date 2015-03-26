b6897e6a2501d582f8face82edf46f99e6c95cec
alexmouquet <alexandre.mouquet@gmail.com>
2015-03-26 16:00:41 +0100

    webpage without features

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>FabLab Isen</title>
+    <link rel="stylesheet" href="css/index.css" />
+	
+</head>
+
+<form method="get" action="index.php">
+
+<body>
+	
+	<img src="header.jpg" class="coin">
+	
+	<header>
+		<a id="headertitle" href="index.php?action=main" style="text-decoration : none">
+		Bienvenue sur le <strong>FabLab Isen Lille</strong>
+		</a>
+	</header>
+
+	
+	
+	<footer>
+        <a href="http://www.isen.fr/lille/"><div>Visit Isen Lille Web site</a></div>
+    </footer>
+</body>
+</html>
